c7184e12cebd7ec381939a059a54c9c57041c1a7
Vlad Stanescu <vladdy.stanescu@gmail.com>
2023-02-06 13:31:39 +0200

    configure github action content changes add i18n for future use

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,16 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 import image from "@astrojs/image";
 
+// import astroI18next from "astro-i18next";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  })]
+  integrations: [
+    tailwind(), 
+    mdx(), 
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp'
+    }),
+    // astroI18next(),
+  ],
 });
