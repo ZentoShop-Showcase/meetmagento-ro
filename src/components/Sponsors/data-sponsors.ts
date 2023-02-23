c2df779f6ace1bea942e43410bf46947622162e0
Vlad Stanescu <vladdy.stanescu@gmail.com>
2023-02-23 15:35:13 +0200

    sponsors page sponsors homepage section

---
+++ src/components/Sponsors/data-sponsors.ts
@@ -0,0 +1,61 @@
+import type { Sponsor } from './sponsor';
+import { SponsorPackage } from './sponsor';
+
+export const SponsorsData: Sponsor[] = [
+    {
+        name: 'ZentoShop',
+        logo: 'assets/sponsors/zentoshop.svg',
+        url: 'https://www.zentoshop.com',
+        package: SponsorPackage.Platinum,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl.',
+    },
+    {
+        name: 'Blugento',
+        logo: 'assets/sponsors/blugento.svg',
+        url: 'https://www.blugento.ro',
+        package: SponsorPackage.Gold,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl.',
+    },
+    {
+        name: 'Blugento',
+        logo: 'assets/sponsors/blugento.svg',
+        url: 'https://www.blugento.ro',
+        package: SponsorPackage.Gold,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl.',
+    },
+    {
+        name: 'eBay',
+        logo: 'assets/sponsors/ebay.svg',
+        url: 'https://www.ebay.com',
+        package: SponsorPackage.Silver,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl.',
+    },
+    {
+        name: 'eBay',
+        logo: 'assets/sponsors/ebay.svg',
+        url: 'https://www.ebay.com',
+        package: SponsorPackage.Silver,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl.',
+    },
+    {
+        name: 'eBay',
+        logo: 'assets/sponsors/ebay.svg',
+        url: 'https://www.ebay.com',
+        package: SponsorPackage.Silver,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl.',
+    },
+    {
+        name: 'eBay',
+        logo: 'assets/sponsors/ebay.svg',
+        url: 'https://www.ebay.com',
+        package: SponsorPackage.Silver,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl.',
+    },
+    {
+        name: 'ZentoShop',
+        logo: 'assets/sponsors/zentoshop.svg',
+        url: 'https://www.zentoshop.com',
+        package: SponsorPackage.Organizer,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl.',
+    },
+];
