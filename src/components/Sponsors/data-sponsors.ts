82ec3862a48036443b380f5897f1e837db9e3a0b
Vlad Stanescu <vladdy.stanescu@gmail.com>
2023-03-02 10:36:02 +0200

    sponsor data

--- src/components/Sponsors/data-sponsors.ts
+++ src/components/Sponsors/data-sponsors.ts
@@ -2,34 +2,27 @@ import type { Sponsor } from './sponsor';
 import { SponsorPackage } from './sponsor';
 
 export const SponsorsData: Sponsor[] = [
-    {
-        name: 'ZentoShop',
-        logo: 'assets/sponsors/zentoshop.svg',
-        url: 'https://www.zentoshop.com',
-        package: SponsorPackage.Platinum,
-        description: 'Zento is a Software-as-a-Service (SaaS) solution built on top of Magento 2 and offers fully managed eCommerce solutions with state-of-the-art Cloud hosting to medium and large merchants.',
-    },
-    {
-        name: 'cyber_Folks',
-        logo: 'assets/sponsors/cyberfolks.svg',
-        url: 'https://cyberfolks.ro/',
-        package: SponsorPackage.Gold,
-        description: 'cyber_Folks is one of the leading hosting providers in the European market, with a large presence in Romania.',
-    },
-    {
-        name: 'Blugento',
-        logo: 'assets/sponsors/blugento.svg',
-        url: 'https://www.blugento.ro',
-        package: SponsorPackage.Gold,
-        description: 'Blugento is a Software-as-a-Service (SaaS) solution built on top of Magento 1 and offers fully managed eCommerce solutions for small a medium merchants.',
-    },
-    {
-        name: 'eBay',
-        logo: 'assets/sponsors/ebay.svg',
-        url: 'https://www.ebay.com',
-        package: SponsorPackage.Silver,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl.',
-    },
+    // {
+    //     name: 'ZentoShop',
+    //     logo: 'assets/sponsors/zentoshop.svg',
+    //     url: 'https://www.zentoshop.com',
+    //     package: SponsorPackage.Platinum,
+    //     description: 'Zento is a Software-as-a-Service (SaaS) solution built on top of Magento 2 and offers fully managed eCommerce solutions with state-of-the-art Cloud hosting to medium and large merchants.',
+    // },
+    // {
+    //     name: 'cyber_Folks',
+    //     logo: 'assets/sponsors/cyberfolks.svg',
+    //     url: 'https://cyberfolks.ro/',
+    //     package: SponsorPackage.Gold,
+    //     description: 'cyber_Folks is one of the leading hosting providers in the European market, with a large presence in Romania.',
+    // },
+    // {
+    //     name: 'Blugento',
+    //     logo: 'assets/sponsors/blugento.svg',
+    //     url: 'https://www.blugento.ro',
+    //     package: SponsorPackage.Gold,
+    //     description: 'Blugento is a Software-as-a-Service (SaaS) solution built on top of Magento 1 and offers fully managed eCommerce solutions for small a medium merchants.',
+    // },
     {
         name: 'eBay',
         logo: 'assets/sponsors/ebay.svg',
@@ -38,18 +31,11 @@ export const SponsorsData: Sponsor[] = [
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl.',
     },
     {
-        name: 'eBay',
-        logo: 'assets/sponsors/ebay.svg',
-        url: 'https://www.ebay.com',
+        name: 'Clever++',
+        logo: 'assets/sponsors/clever.svg',
+        url: 'https://cleverplusplus.ro',
         package: SponsorPackage.Silver,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl.',
-    },
-    {
-        name: 'eBay',
-        logo: 'assets/sponsors/ebay.svg',
-        url: 'https://www.ebay.com',
-        package: SponsorPackage.Silver,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, vel aliquam nunc nisl euismod nisl.',
+        description: "Clever++ is a Magento provider and solution specialist.",
     },
     {
         name: 'ZentoShop',
