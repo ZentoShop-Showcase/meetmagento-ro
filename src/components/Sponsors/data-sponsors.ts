fae1a58e38c53f58bd775b169665c19f240c6124
Vlad Stanescu <vladdy.stanescu@gmail.com>
2023-03-06 11:10:06 +0200

    update clever++ logo

--- src/components/Sponsors/data-sponsors.ts
+++ src/components/Sponsors/data-sponsors.ts
@@ -40,7 +40,7 @@ export const SponsorsData: Sponsor[] = [
     {
         name: 'Clever++',
         logo: 'assets/sponsors/clever.svg',
-        url: 'https://cleverplusplus.ro',
+        url: 'https://cleverplusplus.com/',
         package: SponsorPackage.Silver,
         description: "Clever++ is a Magento provider and solution specialist.",
     },
