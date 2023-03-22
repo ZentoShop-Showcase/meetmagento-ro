dbd2c7c92e301d14897ebe623e11d0d67bd53abc
Vlad Stanescu <vladdy.stanescu@gmail.com>
2023-03-22 11:57:50 +0200

    speakers section speakers page work-in-progress slider with swiper

---
+++ src/components/Speaker/data-mm23ro.ts
@@ -0,0 +1,45 @@
+import type { Speaker } from './speaker';
+import { Track } from './speaker';
+
+export const SpeakersData: Speaker[] = [
+    {
+        name: 'Ray Bogman',
+        photo: 'assets/speakers/mm23ro/raybogman.png',
+        company: 'Adobe',
+        companyTitle: 'Head of Commerce, Customer Engineering EMEA',
+        track: Track.Technical,
+        featured: true,
+    },
+    {
+        name: 'Marius Străjeru',
+        photo: 'assets/speakers/mm23ro/mariusstrajeru.jpg',
+        company: 'Arnia',
+        companyTitle: undefined,
+        track: Track.Technical,
+        featured: true,
+    },
+    {
+        name: 'Răzvan Bradea',
+        photo: 'assets/speakers/mm23ro/razvanbradea.jpg',
+        company: 'Adobe',
+        companyTitle: undefined,
+        track: Track.Technical,
+        featured: true,
+    },
+    {
+        name: 'Ciprian Cazacu',
+        photo: 'assets/speakers/mm23ro/cipriancazacu.png',
+        company: 'easySales',
+        companyTitle: undefined,
+        track: Track.Business,
+        featured: false,
+    },
+    {
+        name: 'Alexandru Balaci',
+        photo: 'assets/speakers/mm23ro/alexandrubalaci.jpg',
+        company: 'Revo',
+        companyTitle: undefined,
+        track: Track.Business,
+        featured: true,
+    },
+];
