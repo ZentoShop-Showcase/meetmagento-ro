28769ffeceafc64349a8ea4a1e8f42cff15ae26f
Vlad Stanescu <vladdy.stanescu@gmail.com>
2023-04-11 15:58:49 +0200

    hyva sponsor

--- src/components/Sponsors/data-sponsors.ts
+++ src/components/Sponsors/data-sponsors.ts
@@ -94,6 +94,13 @@ export const SponsorsData: Sponsor[] = [
         description: 'Romanian privately owned software development and IT consulting company specialized in nearshore and offshore digital transformation with functional expertise in E-commerce, Trading, Banking, Insurance, and Automotive industry.',
     },
     {
+        name: 'Hyvä',
+        logo: '/assets/sponsors/hyva.svg',
+        url: 'https://hyva.io/',
+        package: SponsorPackage.Gold,
+        description: 'Hyvä is the built-from-scratch Magento 2 front end that makes your online store ultra-fast and performant. Hyvä saves you tons of development time and has the fastest ROI in the market, as your traffic, bounce rate and conversion rates are improved.',
+    },
+    {
         name: 'ZentoShop',
         logo: 'assets/sponsors/zentoshop.svg',
         url: 'https://www.zentoshop.com',
